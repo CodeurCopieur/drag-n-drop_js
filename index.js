ef1b67e05e3ee4e4a683648b81af24431b9035c8
codwerk <louis_widdy@live.fr>
2020-10-07 00:16:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const list_items = document.querySelectorAll('.list-item');
+const lists = document.querySelectorAll('.list');
+
+let draggedItem = null;
+
+for (let i = 0; i < list_items.length; i++) {
+  const item = list_items[i];
+
+  for (let j = 0; j < lists.length; j++) {
+    const list = lists[j];
+    
+  }
+  
+}
